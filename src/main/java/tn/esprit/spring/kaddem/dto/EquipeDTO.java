634fdee84fc7d6a46f119c200ec98ab590f1b1f4
Zeineb Haraketi <61319733+ZeinebHaraketi@users.noreply.github.com>
2023-11-11 22:06:23 +0100

    fixes

--- src/main/java/tn/esprit/spring/kaddem/dto/EquipeDTO.java
+++ src/main/java/tn/esprit/spring/kaddem/dto/EquipeDTO.java
@@ -1,46 +1,18 @@
 package tn.esprit.spring.kaddem.dto;
 
 import tn.esprit.spring.kaddem.entities.Niveau;
+import lombok.*;
 
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class EquipeDTO {
     private Integer idEquipe;
     private String nomEquipe;
     private Niveau niveau;
 
-    // Constructors, getters, and setters
 
-    public EquipeDTO() {
-    }
-
-    public EquipeDTO(Integer idEquipe, String nomEquipe, Niveau niveau) {
-        this.idEquipe = idEquipe;
-        this.nomEquipe = nomEquipe;
-        this.niveau = niveau;
-    }
-
-    // Getters and setters
-    public Integer getIdEquipe() {
-        return idEquipe;
-    }
-
-    public void setIdEquipe(Integer idEquipe) {
-        this.idEquipe = idEquipe;
-    }
-
-    public String getNomEquipe() {
-        return nomEquipe;
-    }
-
-    public void setNomEquipe(String nomEquipe) {
-        this.nomEquipe = nomEquipe;
-    }
-
-    public Niveau getNiveau() {
-        return niveau;
-    }
-
-    public void setNiveau(Niveau niveau) {
-        this.niveau = niveau;
-    }
 
 }
