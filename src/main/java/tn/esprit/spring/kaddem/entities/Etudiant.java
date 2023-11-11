e22d605666347b80a7663ce79800c88ca5dd4e16
Zeineb Haraketi <61319733+ZeinebHaraketi@users.noreply.github.com>
2023-11-11 21:48:06 +0100

    fixes

--- src/main/java/tn/esprit/spring/kaddem/entities/Etudiant.java
+++ src/main/java/tn/esprit/spring/kaddem/entities/Etudiant.java
@@ -2,6 +2,10 @@ package tn.esprit.spring.kaddem.entities;
 
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.io.Serializable;
 import java.util.List;
@@ -12,6 +16,10 @@ import javax.persistence.*;
 
 @SuppressWarnings("SpellCheckingInspection")
 @Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Etudiant implements Serializable{
     @Id
     @GeneratedValue(strategy= GenerationType.IDENTITY)
@@ -22,85 +30,13 @@ public class Etudiant implements Serializable{
     private Option op;
     @OneToMany(mappedBy="etudiant", cascade = CascadeType.ALL)
     @JsonIgnore
-    private Set<Contrat> Contrats;
+    private Set<Contrat> contrats;
     @ManyToOne
     @JsonIgnore
     private Departement departement;
-  //  @ManyToMany(cascade =CascadeType.ALL)
     @ManyToMany(mappedBy="etudiants")
 
     @JsonIgnore
     private List<Equipe> equipes ;
-    public Etudiant() {
-    }
-
-    public Etudiant(String nomE, String prenomE) {
-        this.nomE = nomE;
-        this.prenomE = prenomE;
-    }
-
-    public Etudiant(String nomE, String prenomE, Option op) {
-        super();
-        this.nomE = nomE;
-        this.prenomE = prenomE;
-        this.op = op;
-    }
-
-    public Etudiant(Integer idEtudiant, String nomE, String prenomE, Option op) {
-        super();
-        this.idEtudiant = idEtudiant;
-        this.nomE = nomE;
-        this.prenomE = prenomE;
-        this.op = op;
-    }
-
-    public Set<Contrat> getContrats() {
-        return Contrats;
-    }
-
-    public void setContrats(Set<Contrat> contrats) {
-        Contrats = contrats;
-    }
-
-    public Departement getDepartement() {
-        return departement;
-    }
-
-    public void setDepartement(Departement departement) {
-        this.departement = departement;
-    }
-
-    public List<Equipe> getEquipes() {
-        return equipes;
-    }
-
-    public void setEquipes(List<Equipe> equipes) {
-        this.equipes = equipes;
-    }
-
-    public Integer getIdEtudiant() {
-        return idEtudiant;
-    }
-    public void setIdEtudiant(Integer idEtudiant) {
-        this.idEtudiant = idEtudiant;
-    }
-    public String getNomE() {
-        return nomE;
-    }
-    public void setNomE(String nomE) {
-        this.nomE = nomE;
-    }
-    public String getPrenomE() {
-        return prenomE;
-    }
-    public void setPrenomE(String prenomE) {
-        this.prenomE = prenomE;
-    }
-    public Option getOp() {
-        return op;
-    }
-    public void setOp(Option op) {
-        this.op = op;
-    }
-
+    
 }
