388c159d1f1f2570dcd24901a3302659f1702487
Zeineb Haraketi <61319733+ZeinebHaraketi@users.noreply.github.com>
2023-11-11 20:31:49 +0100

    Update EtudiantServiceImpl.java

--- src/main/java/tn/esprit/spring/kaddem/services/EtudiantServiceImpl.java
+++ src/main/java/tn/esprit/spring/kaddem/services/EtudiantServiceImpl.java
@@ -1,4 +1,6 @@
 package tn.esprit.spring.kaddem.services;
+import javax.persistence.EntityNotFoundException;
+
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -16,7 +18,7 @@ import tn.esprit.spring.kaddem.repositories.EtudiantRepository;
 
 import javax.transaction.Transactional;
 import java.util.List;
-import java.util.Set;
+import java.util.Optional;
 
 @Service
 @Slf4j
@@ -41,29 +43,58 @@ public class EtudiantServiceImpl implements IEtudiantService{
 		return etudiantRepository.save(e);
 	}
 
-	public Etudiant retrieveEtudiant(Integer  idEtudiant){
-		return etudiantRepository.findById(idEtudiant).get();
+
+
+	public Etudiant retrieveEtudiant(Integer idEtudiant) {
+		return etudiantRepository.findById(idEtudiant)
+				.orElseThrow(() -> new EntityNotFoundException("Etudiant not found with id: " + idEtudiant));
 	}
+
 
 	public void removeEtudiant(Integer idEtudiant){
 	Etudiant e=retrieveEtudiant(idEtudiant);
 	etudiantRepository.delete(e);
 	}
 
-	public void assignEtudiantToDepartement (Integer etudiantId, Integer departementId){
-        Etudiant etudiant = etudiantRepository.findById(etudiantId).orElse(null);
-        Departement departement = departementRepository.findById(departementId).orElse(null);
-        etudiant.setDepartement(departement);
-        etudiantRepository.save(etudiant);
+	public void assignEtudiantToDepartement(Integer etudiantId, Integer departementId) {
+		Optional<Etudiant> etudiantOptional = etudiantRepository.findById(etudiantId);
+		Optional<Departement> departementOptional = departementRepository.findById(departementId);
+
+		if (!etudiantOptional.isPresent()) {
+			throw new EntityNotFoundException("Etudiant not found with id: " + etudiantId);
+		}
+
+		if (!departementOptional.isPresent()) {
+			throw new EntityNotFoundException("Departement not found with id: " + departementId);
+		}
+
+		Etudiant etudiant = etudiantOptional.get();
+		Departement departement = departementOptional.get();
+
+		etudiant.setDepartement(departement);
+		etudiantRepository.save(etudiant);
 	}
+
+
+
+
 	@Transactional
-	public Etudiant addAndAssignEtudiantToEquipeAndContract(Etudiant e, Integer idContrat, Integer idEquipe){
-		Contrat c=contratRepository.findById(idContrat).orElse(null);
-		Equipe eq=equipeRepository.findById(idEquipe).orElse(null);
+	public Etudiant addAndAssignEtudiantToEquipeAndContract(Etudiant e, Integer idContrat, Integer idEquipe) {
+		Contrat c = contratRepository.findById(idContrat)
+				.orElseThrow(() -> new EntityNotFoundException("Contract not found with id: " + idContrat));
+		Equipe eq = equipeRepository.findById(idEquipe)
+				.orElseThrow(() -> new EntityNotFoundException("Equipe not found with id: " + idEquipe));
+
 		c.setEtudiant(e);
 		eq.getEtudiants().add(e);
-return e;
+
+		contratRepository.save(c);
+		equipeRepository.save(eq);
+
+		return e;
 	}
+
+
 
 	public 	List<Etudiant> getEtudiantsByDepartement (Integer idDepartement){
 return  etudiantRepository.findEtudiantsByDepartement_IdDepart((idDepartement));
