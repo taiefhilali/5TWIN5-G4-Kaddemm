e5d520efe366e68723b81c398fb726865afbb11c
Zeineb Haraketi <61319733+ZeinebHaraketi@users.noreply.github.com>
2023-11-11 19:47:32 +0100

    Create EtudiantDTO.java

---
+++ src/main/java/tn/esprit/spring/kaddem/DTO/EtudiantDTO.java
@@ -0,0 +1,53 @@
+package tn.esprit.spring.kaddem.dto;
+
+import tn.esprit.spring.kaddem.entities.Option;
+
+public class EtudiantDTO {
+    private Integer idEtudiant;
+    private String nomE;
+    private String prenomE;
+    private Option op;
+
+
+    public EtudiantDTO() {super();
+    }
+
+    public EtudiantDTO(Integer idEtudiant, String nomE, String prenomE, Option op) {
+        this.idEtudiant = idEtudiant;
+        this.nomE = nomE;
+        this.prenomE = prenomE;
+        this.op = op;
+    }
+
+    public Integer getIdEtudiant() {
+        return idEtudiant;
+    }
+
+    public void setIdEtudiant(Integer idEtudiant) {
+        this.idEtudiant = idEtudiant;
+    }
+
+    public String getNomE() {
+        return nomE;
+    }
+
+    public void setNomE(String nomE) {
+        this.nomE = nomE;
+    }
+
+    public String getPrenomE() {
+        return prenomE;
+    }
+
+    public void setPrenomE(String prenomE) {
+        this.prenomE = prenomE;
+    }
+
+    public Option getOp() {
+        return op;
+    }
+
+    public void setOp(Option op) {
+        this.op = op;
+    }
+}
