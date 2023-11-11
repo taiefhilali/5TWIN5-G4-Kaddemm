778db6e8392ccfbcc5e7b3aba4e8fd7ad7652cdb
Zeineb Haraketi <61319733+ZeinebHaraketi@users.noreply.github.com>
2023-11-11 20:59:03 +0100

    fixes

---
+++ src/main/java/tn/esprit/spring/kaddem/dto/DepartementDTO.java
@@ -0,0 +1,32 @@
+package tn.esprit.spring.kaddem.dto;
+
+public class DepartementDTO {
+    private Integer idDepart;
+    private String nomDepart;
+
+    public DepartementDTO() {
+    }
+
+    public DepartementDTO(Integer idDepart, String nomDepart) {
+        this.idDepart = idDepart;
+        this.nomDepart = nomDepart;
+    }
+
+    public Integer getIdDepart() {
+        return idDepart;
+    }
+
+    public void setIdDepart(Integer idDepart) {
+        this.idDepart = idDepart;
+    }
+
+    public String getNomDepart() {
+        return nomDepart;
+    }
+
+    public void setNomDepart(String nomDepart) {
+        this.nomDepart = nomDepart;
+    }
+
+
+}
