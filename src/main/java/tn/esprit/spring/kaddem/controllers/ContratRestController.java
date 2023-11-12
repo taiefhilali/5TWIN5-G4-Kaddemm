ab3377490234665e8f87954e2bb03b6c3c7cddd0
Zeineb Haraketi <61319733+ZeinebHaraketi@users.noreply.github.com>
2023-11-12 22:11:06 +0100

    Update ContratRestController.java

--- src/main/java/tn/esprit/spring/kaddem/controllers/ContratRestController.java
+++ src/main/java/tn/esprit/spring/kaddem/controllers/ContratRestController.java
@@ -1,6 +1,5 @@
 package tn.esprit.spring.kaddem.controllers;
 
-import lombok.AllArgsConstructor;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.web.bind.annotation.*;
