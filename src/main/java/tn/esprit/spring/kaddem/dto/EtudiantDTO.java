f0884fade17eb071129a44cfdb7e1459c86a090a
siwar enfaiedh <siwar.enfaiedh@esprit.tn>
2023-11-13 20:29:05 +0100

    committ

--- src/main/java/tn/esprit/spring/kaddem/dto/EtudiantDTO.java
+++ src/main/java/tn/esprit/spring/kaddem/dto/EtudiantDTO.java
@@ -1,55 +1,22 @@
 package tn.esprit.spring.kaddem.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 import tn.esprit.spring.kaddem.entities.Option;
-
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class EtudiantDTO {
     private Integer idEtudiant;
     private String nomE;
     private String prenomE;
     private Option op;
 
-    // Constructors, getters, and setters
 
-    public EtudiantDTO() {
-    }
 
-    public EtudiantDTO(Integer idEtudiant, String nomE, String prenomE, Option op) {
-        this.idEtudiant = idEtudiant;
-        this.nomE = nomE;
-        this.prenomE = prenomE;
-        this.op = op;
-    }
 
-    // Getters and setters
-    public Integer getIdEtudiant() {
-        return idEtudiant;
-    }
 
-    public void setIdEtudiant(Integer idEtudiant) {
-        this.idEtudiant = idEtudiant;
-    }
-
-    public String getNomE() {
-        return nomE;
-    }
-
-    public void setNomE(String nomE) {
-        this.nomE = nomE;
-    }
-
-    public String getPrenomE() {
-        return prenomE;
-    }
-
-    public void setPrenomE(String prenomE) {
-        this.prenomE = prenomE;
-    }
-
-    public Option getOp() {
-        return op;
-    }
-
-    public void setOp(Option op) {
-        this.op = op;
-    }
 }
