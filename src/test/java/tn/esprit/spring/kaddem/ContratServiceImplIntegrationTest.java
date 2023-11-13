ee5146254474e7e48d183891d68cfe91558b4ed2
taiief <62665645+taiefhilali@users.noreply.github.com>
2023-11-13 09:54:06 +0100

    Update ContratServiceImplIntegrationTest.java

--- src/test/java/tn/esprit/spring/kaddem/ContratServiceImplIntegrationTest.java
+++ src/test/java/tn/esprit/spring/kaddem/ContratServiceImplIntegrationTest.java
@@ -23,7 +23,7 @@ import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.when;
 
 @SpringBootTest
-@RunWith(MockitoJUnitRunner.class)
+// @RunWith(MockitoJUnitRunner.class)
 
 class ContratServiceImplIntegrationTest {
 
